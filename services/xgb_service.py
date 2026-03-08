bc6fef032fda91483ba9ab2f67c2e50bc6e7bb59
nikhilgowdawork <nikhilgowda.work@gmail.com>
2026-03-08 21:39:06 +0530

    working on SHAP

--- services/xgb_service.py
+++ services/xgb_service.py
@@ -1,6 +1,10 @@
 import os
 from sklearn.preprocessing import StandardScaler
 import joblib
+import shap
+import pandas as pd
+import numpy as np
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) #finding the path in the whole folder
 
@@ -12,14 +16,26 @@ scaler_path = os.path.join(BASE_DIR,"models","scaler.pkl")
 model = joblib.load(model_path)
 scaler = joblib.load(scaler_path)
 
+#Implementing SHAP to explain that "WHY"
+explainer = shap.TreeExplainer(model)
+
 
 def XGBoostcome(transaction):
     transaction_scaled = scaler.transform(transaction)
 
     prob = model.predict_proba(transaction_scaled)[:,1]
-
+    
     threshold = 0.6
+    
     decision = (prob >= threshold).astype(int)
+
+    #SHAP Explaination
+    shap_values = explainer.shap_values(transaction_scaled)
+
+    feature_impact = pd.DataFrame({
+        "feature": transaction.columns,
+        "impact" : shap_values[0]
+    })
 
     return prob ,decision
 
